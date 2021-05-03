9dc4494b0e3e81bf4700c8e2f949b1d81e1df2c6
saidwho12 <Jarzabi2020@gmail.com>
2021-05-03 02:45:14 -0400

    Changed compiler flags, wrote example and updated README

--- README.md
+++ README.md
@@ -1,11 +1,70 @@
 # Hamza
-Hamza is a lightweight, fast and portable C89 [OpenType](https://docs.microsoft.com/en-us/typography/opentype/spec) library.
-It was written primarily for Arabic but [Unicode](https://unicode-table.com)/[OpenType](https://docs.microsoft.com/en-us/typography/opentype/spec) are very general so it does work with other scripts. That is as long as you are not using special features which are not implemented for a specific writing system. Works well with Arabic, Greek, Latin and Roman writing systems. Han, Indic and Cyrillic writing systems should *hopefully* be supported soon.
+Hamza is a lightweight, fast and portable C89 (ANSI C) [OpenType](https://docs.microsoft.com/en-us/typography/opentype/spec) library.
+It was written primarily for Arabic but [Unicode](https://unicode-table.com)/[OpenType](https://docs.microsoft.com/en-us/typography/opentype/spec) are very general so it does work with other scripts. That is as long as you are not using special features which are not implemented for a specific writing system. Works well with Arabic, Greek, Latin and Roman writing systems. Han, Indic and Cyrillic writing systems should *hopefully* be supported soon. Note that this is _**not**_ a renderer, it handles shaping only.
 
-![Example of Arabic rendered using Hamza](render.png)
+![Example of Complex Arabic rendered using Hamza](render.png)
 
-# Documentation
-https://saidwho12.github.io/hamza/
+
+## Basic Usage
+This is a minimal "Hello, World!" example using Hamza. For more detailed usage information, refer to the [docs](https://saidwho12.github.io/hamza/).
+```c
+#include <hz.h>
+#include <hz-ft.h>
+
+int main(int argc, char *argv[]) {
+    /* variable declarations */
+    FT_Library ft_library;
+    FT_Face ft_face;
+    hz_font_t *font;
+    hz_sequence_t *sequence;
+    hz_context_t *context;
+    hz_array_t *features;
+    
+    /* initialize variables state */
+    if (FT_Init_FreeType(&ft_library)) {
+        fputs("Failed to load FreeType!\n", stderr);
+        goto failed_to_load_freetype;
+    }
+    
+    if (FT_New_Face(ft_library, "NotoSans-Regular.ttf", 0, &ft_face)) {
+        fputs("Failed to load Font!\n", stderr);
+        goto failed_to_load_face;
+    }
+    
+    font = hz_ft_font_create(ft_face);
+    context = hz_context_create(font);
+    
+    /* set typographic features */
+    features = hz_array_create();
+    
+    hz_context_set_direction(context, HZ_DIRECTION_LTR);
+    hz_context_set_script(context, HZ_SCRIPT_LATIN);
+    hz_context_set_language(context, HZ_LANGUAGE_ENGLISH);
+    hz_context_set_features(context, features);
+    
+    sequence = hz_sequence_create();
+    hz_sequence_load_utf8(sequence, "Hello, World!");
+    
+    /* shape the actual sequence of glyphs */
+    hz_shape_full(context, sequence);
+    
+    /* render (using FreeType, or your method of choice) */
+    
+    /* free resources */
+    failed_to_load_face:
+    FT_Done_FreeType(ft_library);
+    failed_to_load_freetype:
+    return 0;
+}
+
+
+```
+
+## Bindings
+There are no bindings yet for Hamza, but I have planned to work on a python one soon.
+
+## Build
+
 
 ## TODO
 - [x] RTL
--- main.c
+++ main.c
@@ -1,171 +1,48 @@
 #include <hz.h>
 #include <hz-ft.h>
 
-#include <inttypes.h>
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-
-//typedef uint8_t BYTE;
-//typedef uint32_t DWORD;
-//
-//struct DDPIXELFORMAT {
-//    DWORD dwSize;
-//    DWORD dwFlags;
-//    DWORD dwFourCC;
-//    DWORD dwRGBBitCount;
-//    DWORD dwRBitMask;
-//    DWORD dwGBitMask;
-//    DWORD dwBBitMask;
-//    DWORD dwRGBAlphaBitMask;
-//};
-//
-//struct DDCAPS2 {
-//    DWORD dwCaps1;
-//    DWORD dwCaps2;
-//    DWORD Reserved[2];
-//};
-//
-//struct DDSURFACEDESC2 {
-//    DWORD dwSize;
-//    DWORD dwFlags;
-//    DWORD dwHeight;
-//    DWORD dwWidth;
-//    DWORD dwPitchOrLinearSize;
-//    DWORD dwDepth;
-//    DWORD dwMipMapCount;
-//    DWORD dwReserved1[11];
-//    DDPIXELFORMAT ddpfPixelFormat;
-//    DDCAPS2 ddsCaps;
-//    DWORD dwReserved2;
-//};
-//
-//struct DDSFILE {
-//    DWORD dwMagic;
-//    DDSURFACEDESC2 ddsd;
-//    BYTE *bData1;
-//    BYTE *bData2;
-//};
-
-#define WIDTH 800
-#define HEIGHT 200
-
-int blit(uint8_t *dst, int dw, int dh, int bpp,
-          const uint8_t *src,
-          int sx, int sy, int sw, int sh)
-{
-    int y;
-    int ss = sw * bpp; /* source stride */
-    int ds = dw * bpp; /* destination stride */
-
-    /* bounds check */
-    if (!(sx >= 0 && (sx + sw) < dw && sy >= 0 && (sy + sh) < dh)) {
-        return 0;
-    }
-
-    for (y = 0; y < sh; ++y) {
-        const unsigned char *sl = src + y * ss;
-        unsigned char *dl = dst + (sx + y) * ds + sy * bpp;
-        memcpy(dl, sl, ss);
-    }
-
-    return 1;
-}
-
 int main(int argc, char *argv[]) {
-    const char *text = "لاَ تَمْشِ فِي الأَرْضِ مَرَحًا إِنَّكَ لَن تَخْرِقَ الأَرْضَ وَلَن تَبْلُغَ الْجِبَالَ طُولاً";
-
-    // Load font using FreeType
+    /* variable declarations */
     FT_Library ft_library;
-    FT_Init_FreeType(&ft_library);
-
     FT_Face ft_face;
-    FT_New_Face(ft_library, "../../../data/fonts/UthmanicHafs1 Ver13.ttf", 0, &ft_face);
-    FT_Set_Char_Size(ft_face, 1000, 0, 0, 0);
+    hz_font_t *font;
+    hz_sequence_t *sequence;
+    hz_context_t *context;
+    hz_array_t *features;
 
-
-    // setup shaping context and buffer
-    hz_font_t *font = hz_ft_font_create(ft_face);
-
-    // setup language features
-    hz_array_t *features = hz_array_create();
-    hz_array_push_back(features, HZ_FEATURE_CCMP);
-    hz_array_push_back(features, HZ_FEATURE_ISOL);
-    hz_array_push_back(features, HZ_FEATURE_FINA);
-    hz_array_push_back(features, HZ_FEATURE_MEDI);
-    hz_array_push_back(features, HZ_FEATURE_INIT);
-    hz_array_push_back(features, HZ_FEATURE_RLIG);
-    hz_array_push_back(features, HZ_FEATURE_CALT);
-    hz_array_push_back(features, HZ_FEATURE_LIGA);
-    hz_array_push_back(features, HZ_FEATURE_DLIG);
-
-    // positional features
-    hz_array_push_back(features, HZ_FEATURE_MARK);
-    hz_array_push_back(features, HZ_FEATURE_MKMK);
-
-
-    hz_context_t *ctx = hz_context_create(font);
-    hz_context_set_direction(ctx, HZ_DIRECTION_RTL);
-    hz_context_set_script(ctx, HZ_SCRIPT_ARABIC);
-    hz_context_set_language(ctx, HZ_LANGUAGE_ARABIC);
-    hz_context_set_features(ctx, features);
-
-    // Load utf8 string "_zt" stands for zero-terminated
-    hz_sequence_t *section = hz_sequence_create();
-    hz_sequence_load_utf8(section, (const hz_char *) text);
-    hz_shape_full(ctx, section);
-
-
-    uint8_t *image = malloc(WIDTH * HEIGHT);
-    memset(image,0, WIDTH * HEIGHT);
-
-    int xpos = 400, ypos = 100;
-
-    hz_sequence_node_t *node = ctx->dir == HZ_DIRECTION_RTL ? hz_sequence_last_node(section->root) : section->root;
-    while (node != NULL) {
-        FT_GlyphSlot slot = ft_face->glyph;
-        FT_Glyph glyph;
-
-        FT_Load_Glyph(ft_face, node->glyph.id, FT_LOAD_DEFAULT);
-//        FT_Get_Glyph(slot, &glyph);
-        FT_Render_Glyph(slot, FT_RENDER_MODE_NORMAL);
-
-        unsigned int w = slot->bitmap.width;
-        unsigned int h = slot->bitmap.rows;
-        int xb = slot->bitmap_left >> 6;
-        int yb = slot->bitmap_top >> 6;
-        int xo = node->x_offset >> 6;
-        int yo = node->y_offset >> 6;
-
-        uint16_t x0 = xpos + xo + xb;
-        uint16_t y0 = ypos + yo + (h - yb);
-        /*
-        uint16_t x1 = x0 + w;
-        uint16_t y1 = y0 + h;
-        */
-        int tw = pow(2, ceil(log(w)/log(2)));
-        int th = pow(2, ceil(log(h)/log(2)));
-
-        uint8_t *tdata = malloc(tw * th);
-        int iy;
-
-        for(iy = 0; iy < h; ++iy) {
-            memcpy(tdata + iy * tw, slot->bitmap.buffer + iy * w, h);
-        }
-
-        blit(image, WIDTH, HEIGHT, 1, tdata, x0, y0, w, h);
-
-        free(tdata);
-
-//        FT_Done_Glyph(glyph);
-
-        xpos += node->glyph.x_advance;
-        node = ctx->dir == HZ_DIRECTION_RTL ? node->prev : node->next;
+    /* initialize variables state */
+    if (FT_Init_FreeType(&ft_library)) {
+        fputs("Failed to load FreeType!\n", stderr);
+        goto failed_to_load_freetype;
     }
 
-    stbi_write_bmp("./example.bmp", WIDTH, HEIGHT, 1, image);
-    free(image);
+    if (FT_New_Face(ft_library, "NotoSans-Regular.ttf", 0, &ft_face)) {
+        fputs("Failed to load Font!\n", stderr);
+        goto failed_to_load_face;
+    }
 
+    font = hz_ft_font_create(ft_face);
+    context = hz_context_create(font);
 
+    /* set typographic features */
+    features = hz_array_create();
+
+    hz_context_set_direction(context, HZ_DIRECTION_LTR);
+    hz_context_set_script(context, HZ_SCRIPT_LATIN);
+    hz_context_set_language(context, HZ_LANGUAGE_ENGLISH);
+    hz_context_set_features(context, features);
+
+    sequence = hz_sequence_create();
+    hz_sequence_load_utf8(sequence, "Hello, World!");
+
+    /* shape the actual sequence of glyphs */
+    hz_shape_full(context, sequence);
+
+    /* render (using FreeType, or your method of choice) */
+
+    /* free resources */
+    failed_to_load_face:
+    FT_Done_FreeType(ft_library);
+    failed_to_load_freetype:
     return 0;
 }

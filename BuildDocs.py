f0d59f4e651951ddee14202bfc500755fb7b71d3
saidwho13 <Jarzabi2020@gmail.com>
2023-03-07 10:58:20 -0500

    Fixed build docs script and renamed it.

--- BuildDocs.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-import os
-
-print(os.name)
-os.system("\"C:\\Program Files (x86)\\Natural Docs\\NaturalDocs.exe\" -p .\\nd-project -i .\\hz -o HTML .\\docs\\ --tab-width 4 --no-auto-group --rebuild")
